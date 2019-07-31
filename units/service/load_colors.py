86df3cd661109e48781c17c934c220932ed630d6
Wurka <wurka_13@mail.ru>
2019-07-31 11:09:30 +0300

    added valid eskiz colors

--- units/service/load_colors.py
+++ units/service/load_colors.py
@@ -11,31 +11,31 @@ if input("for continue print y: ").lower() == "y":
 
 	# исходные данные для заполнения базы
 	group_1 = [  # яркие цвета
-		'23f81d', 'f465ea', 'd2cd8a', '7b4c93', 'f68592',
-		'aeac64', '06d415', '221393', 'bd1426', '111f42',
-		'bbef7e', 'f84c3f', '43e721', 'b8d250', '316a00',
-		'd1e87a', '4b0f47', 'cbae48', '9f0134', '212b10'
+		'FF0600', 'FF0063', 'DA00FF', '6400FF', '0025FF',
+		'0057FF', '00A4FF', '0ADEEC', '00F4AB', '00D622',
+		'89F900', 'CDEA00', 'FFEB00', 'FFB900', 'FF8700',
+		'F85900', '75554A', '9C9C9C', '677077', '000000'
 	]
 
 	group_2 = [  # тусклые цвета
-		'd5d9c9', '03e3d1', 'e2d174', 'f987ac', 'da8eb9',
-		'b084ca', 'a3fac0', 'fd2aab', 'ea9076', 'd65eae',
-		'47ea69', '48f348', '6784e9', '9c3785', '89c74f',
-		'e2692c', '9b4001', '5ac053', '87d964', '9af1a5'
+		'FFB5B3', 'FFB3D1', 'F4B3FF', 'D1B3FF', 'B3BEFF',
+		'B3CDFF', 'B3E4FF', 'B6F6FA', 'B3FCE6', 'B3F3BD',
+		'DCFEB3', 'F0F9B3', 'FFFAB3', 'FFEBB3', 'FFDCB3',
+		'FDCEB3', 'D6CCC9', 'E2E2E2', 'D2D5D7', 'FFFFFF'
 	]
 
-	group_3 = ['e8f862', 'b03d80', '88c14c', '947435', 'f2614a']  # цвета кожи скальпа
+	group_3 = ['F2EADD', 'F7E3CB', 'FFF3DB', 'FFF9E7', 'FFF4F3']  # цвета кожи скальпа
 
 	# group_4 = [f"wood{i+1:02}" for i in range(10)]  # дерево
 	group_4 = [
-		"whiterdoob.jpg", "venge.jpg", "italianoreh.jpg", "redwood.jpg",
-		"naturaldoob.jpg", "palicandr.jpg", "whiteoreh.jpg", "darkdoob.jpg", "clearwood.jpg"
+		"whiterdoob.jpg", "clearwood.jpg", "naturaldoob.jpg", "whiteoreh.jpg", "italianoreh.jpg",
+		"redwood.jpg", "palicandr.jpg", "darkdoob.jpg", "venge.jpg", "craft.jpg"
 	]
 
 	# group_5 = [f"metal{i+1:02}" for i in range(10)]  # металл
 	group_5 = [
-		"bronze.jpg", "gold.jpg", "krak.jpg", "craft.jpg", "coper.jpg",
-		"patina.jpg", "rust.jpg", "silver.jpg", "oldgold.jpg", "hz.jpg"
+		"silver.jpg", "gold.jpg", "krak.jpg", "coper.jpg", "rust.jpg",
+		"hz.jpg", "oldgold.jpg", "krak.jpg", "patina.jpg", "glas.jpg"
 	]
 
 	for g in group_1:
